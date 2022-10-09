9bc24d83a512aec8fe524bc8d3ae831cd4e9b72e
happyanran <yuld@qq.com>
2022-10-09 09:41:28 +0800

    token

--- middleware/authMw.go
+++ middleware/authMw.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/happyanran/walnut/common"
+	"github.com/happyanran/walnut/model"
 )
 
 func AuthMw(svcCtx *common.ServiceContext) gin.HandlerFunc {
@@ -25,11 +26,19 @@ func AuthMw(svcCtx *common.ServiceContext) gin.HandlerFunc {
 			return
 		}
 
-		userID := claims.UserId
-
 		//查库 用户状态改变
+		user := &model.User{
+			ID: claims.UserId,
+		}
 
-		c.Set("UserID", userID)
+		if err := user.UserFindByID(); err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"msg": "权限不足"})
+			c.Abort()
+			return
+		}
+
+		c.Set("UserID", user.ID)
+		c.Set("Username", user.Username)
 
 		c.Next()
 	}
